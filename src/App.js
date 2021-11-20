241146c0be8b9349abb1a90a5145f883b0e38b20
Long Vuong <longvuong@live.com>
2021-11-20 12:37:18 -0500

    tri-version

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 // import UserEntry from './UserEntry'
 // import AddMoreRows from './AddMoreRows';
 // import Rows from './Rows';
 
-
-
-
 function App() {
-
   const [rowsOnScreen, setRowsOnScreen] = useState(1);
-  const [rowsToAdd, setRowsToAdd] = useState(0)
-
+  const [rowsToAddValue, setRowsToAddValue] = useState(0);
+  const [rowsToAdd, setRowsToAdd] = useState(0);
 
   const handleChange = (event) => {
-    const x = Number(event.target.value)
+    const x = Number(event.target.value);
     // console.log(x)
     // console.log(typeof(x))
 
-    setRowsToAdd(x)
-  }
+    setRowsToAddValue(x);
+  };
 
   const handleSubmit = () => {
-    console.log(`creating ${rowsToAdd} rows`)
-
-    setRowsToAdd(0);
-
-    return (
-      <>
-        <label htmlFor="">{}row</label>
-        <input type="text" />
-      </>
-    )
-
-  }
+    setRowsToAdd(rowsToAddValue);
+    // setRowsToAddValue(0);
+    // setRowsToAdd(0);
+  };
 
   return (
     <div className="App">
-
-      {/* <Rows /> */}
-
-      <h3> add
-        <input type="number" value={rowsToAdd} onChange={handleChange} />
-        rows to screen
-        <button type="submit" onClick={() => handleSubmit()} >create row</button>
+      <h3>
+        {" "}
+        add
+        <input type="number" value={rowsToAddValue} onChange={handleChange} />
+        more rows to the screen
+        <button type="submit" onClick={() => handleSubmit()}>
+          create row
+        </button>
       </h3>
-
+      {new Array(rowsToAdd).fill(1).map((_, index) => {
+        return <Row key={index} index={index + 1} />;
+      })}
     </div>
   );
 }
 
 export default App;
+
+const Row = ({ index }) => {
+  return (
+    <div>
+      <label>{index}</label>
+      <input type="text" />
+    </div>
+  );
+};
