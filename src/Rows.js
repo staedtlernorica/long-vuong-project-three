e739b3abe55d16bd6ac20bebf067bb3d60ce05a9
Long Vuong <longvuong@live.com>
2021-11-24 21:31:44 -0500

    netlify rows

--- src/Rows.js
+++ src/Rows.js
@@ -1,17 +1,8 @@
-import { useState, useEffect } from "react";
-
 function Rows(props) {
-
-  const [userInput, setUserInput] = useState('');
-
-  // updates what the user typed, and the index/where the user just typed
-  useEffect(() => {
-    props.updateEntry([userInput, props.index])
-  }, [userInput])
 
   return (
     <li>
-      <input type="text" onChange={(e) => setUserInput(e.target.value)} value={userInput} />
+      <input type="text" onChange={(e) => props.updateEntry([e.target.value, props.index])}  />
     </li>
   );
 
