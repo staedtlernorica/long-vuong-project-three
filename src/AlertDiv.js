551fed3a046eabe9cd818197fa9dd7965a3eaf04
Long Vuong <longvuong@live.com>
2022-01-27 00:26:26 -0500

    added no empty pop up div

--- src/AlertDiv.js
+++ src/AlertDiv.js
@@ -7,6 +7,7 @@ function AlertDiv(props) {
         
       </div>
 
+        {/*alert prop.saveOrDeleteAlert is to style the pop up div */}
         <div className={`alert ${props.saveOrDeleteAlert}`}>
           <h2>{props.message}</h2>
         </div>
